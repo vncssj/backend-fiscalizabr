b9b5d4e35d685257fc9da83ea9c19b39fe44117d
vinicius de Jesus <vinicius.corretorparceiro.com.br>
2020-12-07 11:51:45 -0300

    teste

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,51 +1,51 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response) {
-    
-        const usuario = await connection('usuario')
-          .select([
-            'usuario.*'
-          ]);
-    
-        return response.json(usuario);
-      },
-    
-      async indexId(request, response) {
-        const { id } = request.query;
-        
-        const [count] = await connection('usuario').where('id',id).count();
+  async index(request, response) {
+    console.log("index")
+    const usuario = await connection('usuario')
+      .select([
+        'usuario.*'
+      ]);
 
-        const usuario = await connection('usuario')
-          .where('id',id)
-          .select([
-            'usuario.*'
-          ]);
-    
-        response.header('X-Total-Count', count['count(*)']);
-    
-        return response.json(usuario);
-      },
-    
-      async create(request, response) {
-        const { email, senha, nome, cidade, estado } = request.body;
+    return response.json(usuario);
+  },
 
-        const [id] = await connection('usuario').insert({
-            email, 
-            senha,
-            nome, 
-            cidade, 
-            estado
-        });
-    
-        return response.json({ id });
-      },
-    
-      async delete(request, response) {
-        const { id } = request.query;
-    
-        await connection('usuario').where('id', id).delete();
-    
-        return response.status(204).send();
-      }
+  async indexId(request, response) {
+    const { id } = request.query;
+
+    const [count] = await connection('usuario').where('id', id).count();
+
+    const usuario = await connection('usuario')
+      .where('id', id)
+      .select([
+        'usuario.*'
+      ]);
+
+    response.header('X-Total-Count', count['count(*)']);
+
+    return response.json(usuario);
+  },
+
+  async create(request, response) {
+    const { email, senha, nome, cidade, estado } = request.body;
+
+    const [id] = await connection('usuario').insert({
+      email,
+      senha,
+      nome,
+      cidade,
+      estado
+    });
+
+    return response.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.query;
+
+    await connection('usuario').where('id', id).delete();
+
+    return response.status(204).send();
+  }
 };
